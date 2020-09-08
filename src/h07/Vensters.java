963fde46921da01e33d3e56f0bd2e45078e099cc
danilloef2004 <dani.loef@student.rocvf.nl>
2020-09-08 13:03:28 +0200

    .

---
+++ src/h07/Vensters.java
@@ -0,0 +1,11 @@
+//1. Kun je in de Java API (zie web-links) deze methode ergens vinden?
+
+//nee, wel appletResize, wat hetzelfde doet.
+
+//2. In welke klasse wordt deze methode gedefinieerd?
+
+//javaApplet
+
+//3. Dezelfde klasse heeft een tweede methode met dezelfde naam. Wat is het verschil?
+
+//Ze hebben beide verschillende argumenten.
