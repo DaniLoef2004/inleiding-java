e312f44eb6fa6b536a28ac053272c5da99b871be
danilloef2004 <dani.loef@student.rocvf.nl>
2020-09-08 10:11:06 +0200

    .

--- src/h02/JavaTekenenVragen.java
+++ src/h02/JavaTekenenVragen.java
@@ -1,7 +1,31 @@
 //Wat is een platform?
+
+//Dat is de combinatie van de processor en het besturingssysteem
+
 //Waarom hoeft een programmeur in Java maar één keer een programma te schrijven voor verschillende platforms?
+
+//Java wordt automatisch vertaald naar een tussentaal die de standaard is.
+
 //Wat is bytecode?
+
+//Bytecode is de standaard tussentaal die tussen verschillende besturingssystemen wordt gebruikt.
+
 //Wat doet JVM?
+
+//JVM vertaalt de Bytecode naar de beste taal voor het betreffende besturingssysteem wanneer het wordt uitgevoerd.
+
 //Met welke methode kun je een vierkant tekenen?
+
+//4x g.drawline
+//g. drawRect
+//g. fillRect
+
 //Noem drie methodes waarmee je een cirkel kunt tekenen
+
+//g.drawOval
+//g.fillOVal
+//g.drawArc met 360 graden.
+
 //Waarom worden opdrachten voor het scherm in paint() gezet en niet in een andere methode?
+
+//paint() wordt aangeroepen als de applet wordt ververst/herladen.
