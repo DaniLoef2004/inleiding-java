dde0e2896ee6bf7f4f775c3ff72465844795e4aa
danilloef2004 <dani.loef@student.rocvf.nl>
2020-09-08 10:46:33 +0200

    .

---
+++ src/h06/JavaGetallenVragen.java
@@ -0,0 +1,30 @@
+//1. Waarom is initialiseren van variabelen noodzakelijk?
+
+//Anders weet je niet welke waarden in de variabelen staan, en betekenen ze dus eigenlijk niks
+
+
+
+//2. Waarom is er typecasting noodzakelijk bij de conversie van een double naar een int?
+
+//Zodat de compiler begrijpt dat de informatie die verloren kan gaan, ook verloren mag gaan.
+
+
+
+//3. Wat is de uitvoer van dit programma? Geef een verklaring.
+
+//De uitvoering is  public void paint(Graphics g) {
+//                  g.drawString("De uitkomst is: " + uitkomst, 20, 20);
+//
+// Dit omdat paint hetgene is dat daadwerkelijk op het scherm wordt weergeven.
+
+
+
+//4. In de regel uitkomst = (a + b) / c; laat je de ronde haken weg. De regel ziet er dan als volgt uit:
+
+//Dit is toch geen vraag?
+
+
+
+//5. Wat is de uitvoer van dit programma? Geef een verklaring.
+
+//uitkomst, de rest zal niet weergeven worden.
