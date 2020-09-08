b1fb6179054fdce3719caf8be45e156e62340084
danilloef2004 <dani.loef@student.rocvf.nl>
2020-09-08 10:00:50 +0200

    .

---
+++ src/h02/JavaTekenenVragen.java
@@ -0,0 +1,7 @@
+//Wat is een platform?
+//Waarom hoeft een programmeur in Java maar één keer een programma te schrijven voor verschillende platforms?
+//Wat is bytecode?
+//Wat doet JVM?
+//Met welke methode kun je een vierkant tekenen?
+//Noem drie methodes waarmee je een cirkel kunt tekenen
+//Waarom worden opdrachten voor het scherm in paint() gezet en niet in een andere methode?
