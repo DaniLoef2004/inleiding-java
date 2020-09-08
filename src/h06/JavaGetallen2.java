6d8316706728cfdec1684eb0df4c485f6d9be416
danilloef2004 <dani.loef@student.rocvf.nl>
2020-09-08 11:48:47 +0200

    .

---
+++ src/h06/JavaGetallen2.java
@@ -0,0 +1,24 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+public class JavaGetallen2 extends Applet {
+    double Totaalbedrag;
+    double AantalMensen;
+    double Geld;
+
+
+    public void init() {
+        Totaalbedrag = 113;
+        AantalMensen = 4;
+        Geld = Totaalbedrag / AantalMensen;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Dani: €" + Geld, 20, 20);
+        g.drawString("Jan: €" + Geld, 20, 35);
+        g.drawString("Ali: €" + Geld, 20, 50);
+        g.drawString("Jeannette: €" + Geld, 20, 65);
+    }
+}
