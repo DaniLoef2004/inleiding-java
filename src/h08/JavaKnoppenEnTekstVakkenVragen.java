6f4dd5de6bd6a07584b8f38fadb8d4e1dfd2b205
danilloef2004 <dani.loef@student.rocvf.nl>
2020-09-09 12:10:31 +0200

    .

---
+++ src/h08/JavaKnoppenEnTekstVakkenVragen.java
@@ -0,0 +1,23 @@
+//Wat is event-handling?
+
+//De klasse die te maken heeft met de codes van de knoppen en tekstvakken
+
+//Wat is een gebruikersinterface?
+
+//Hetgene wat voor de gebruiker zichtbaar is
+
+//Waarom moet er voor een knop wel en voor een tekst geen event-handling geprogrammeerd worden?
+
+//Omdat er alleen standaard handelingen nodig zijn voor het programmeren van tekst.
+
+//Wat is een lege string?
+
+//Een string zonder iets er in, dit kan gebruikt worden om een lijn leeg te maken of leeg te houden.
+
+//Wat is het verschil tussen een tekstvak en een label?
+
+//Een label kan niks op ingetikt worden, maar wel in geschreven nog kan worden.
+
+//Waarom moeten getallen uit een tekstvak altijd geconverteerd worden naar een double of een int?
+
+//Zodat het in een variabele omgezet kan worden.
