55e4fe0d678373199c52993ce9f4e1f523bf03ef
danilloef2004 <dani.loef@student.rocvf.nl>
2020-09-12 13:51:38 +0200

    .

--- src/h10/JavaBeslissingen2.java
+++ src/h10/JavaBeslissingen2.java
@@ -15,30 +15,29 @@ public class JavaBeslissingen2 extends Applet {
     public void init() {
         tekstvak = new TextField("", 20);
         label = new Label("Type een jaartal en druk op enter");
-        tekstvak.addActionListener( new TekstvakListener() );
+        tekstvak.addActionListener(new TekstvakListener());
         add(label);
         add(tekstvak);
         nieuw = true;
     }
 
     public void paint(Graphics g) {
-        g.drawString(hoogGetal + " Is het hoogste getal.", 50, 60 );
-        g.drawString(laagGetal + " Is het laagste getal.", 50, 75 );
+        g.drawString(hoogGetal + " Is het hoogste getal.", 50, 60);
+        g.drawString(laagGetal + " Is het laagste getal.", 50, 75);
     }
 
     class TekstvakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
             s = tekstvak.getText();
-            nieuwGetal = Integer.parseInt( s);
-            if (nieuw){
+            nieuwGetal = Integer.parseInt(s);
+            if (nieuw) {
                 hoogGetal = nieuwGetal;
                 laagGetal = nieuwGetal;
                 nieuw = false;
                 repaint();
 
-            }
-            else {
+            } else {
                 if (nieuwGetal > hoogGetal) {
                     hoogGetal = nieuwGetal;
                     repaint();
